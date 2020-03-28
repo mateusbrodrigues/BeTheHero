17fa93528f2b0dc87f4d07c9779882f017ac0b94
Mateus Borges <mateusborges090@gmail.com>
2020-03-28 16:05:31 -0300

    back-end

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros,paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utlizado para criar ou alterar recursos
+ */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ * 
+ */
+
+
+
+
